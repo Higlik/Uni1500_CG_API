fe797e2d60434cc50e3c1ed78a8036323dc6f043
TarsilaNascMartins <tarsilawho@gmail.com>
2022-08-13 12:33:43 -0300

    TbBancos e TbFuncionarios

---
+++ CgApi/Controllers/TbBancosController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ExemploEntityFramework;
+using CgApi;
+
+namespace ExemploEntityFramework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TbBancosController : ControllerBase
+    {
+        private readonly cursodev_grupo2Context _context;
+
+        public TbBancosController(cursodev_grupo2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TbBancos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TbBanco>>> GetTbBanco()
+        {
+            if (_context.TbBanco == null)
+            {
+                return NotFound();
+            }
+            return await _context.TbBanco.ToListAsync();
+        }
+
+        // GET: api/TbBancos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TbBanco>> GetTbBanco(int id)
+        {
+            if (_context.TbBanco == null)
+            {
+                return NotFound();
+            }
+            var tbBanco = await _context.TbBanco.FindAsync(id);
+
+            if (tbBanco == null)
+            {
+                return NotFound();
+            }
+
+            return tbBanco;
+        }
+
+        // PUT: api/TbBancos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTbBanco(int id, TbBanco tbBanco)
+        {
+            if (id != tbBanco.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tbBanco).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TbBancoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TbBancos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TbBanco>> PostTbBanco(TbBanco tbBanco)
+        {
+            if (_context.TbBanco == null)
+            {
+                return Problem("Entity set 'cursodev_grupo2Context.TbBanco'  is null.");
+            }
+            _context.TbBanco.Add(tbBanco);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TbBancoExists(tbBanco.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTbBanco", new { id = tbBanco.Id }, tbBanco);
+        }
+
+        // DELETE: api/TbBancos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTbBanco(int id)
+        {
+            if (_context.TbBanco == null)
+            {
+                return NotFound();
+            }
+            var tbBanco = await _context.TbBanco.FindAsync(id);
+            if (tbBanco == null)
+            {
+                return NotFound();
+            }
+
+            _context.TbBanco.Remove(tbBanco);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TbBancoExists(int id)
+        {
+            return (_context.TbBanco?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ CgApi/Controllers/TbFuncionariosController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ExemploEntityFramework;
+using CgApi;
+
+namespace ExemploEntityFramework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TbFuncionariosController : ControllerBase
+    {
+        private readonly cursodev_grupo2Context _context;
+
+        public TbFuncionariosController(cursodev_grupo2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TbFuncionarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TbFuncionario>>> GetTbFuncionario()
+        {
+            if (_context.TbFuncionario == null)
+            {
+                return NotFound();
+            }
+            return await _context.TbFuncionario.ToListAsync();
+        }
+
+        // GET: api/TbFuncionarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TbFuncionario>> GetTbFuncionario(int id)
+        {
+            if (_context.TbFuncionario == null)
+            {
+                return NotFound();
+            }
+            var tbFuncionario = await _context.TbFuncionario.FindAsync(id);
+
+            if (tbFuncionario == null)
+            {
+                return NotFound();
+            }
+
+            return tbFuncionario;
+        }
+
+        // PUT: api/TbFuncionarios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTbFuncionario(int id, TbFuncionario tbFuncionario)
+        {
+            if (id != tbFuncionario.Matricula)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tbFuncionario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TbFuncionarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TbFuncionarios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TbFuncionario>> PostTbFuncionario(TbFuncionario tbFuncionario)
+        {
+            if (_context.TbFuncionario == null)
+            {
+                return Problem("Entity set 'cursodev_grupo2Context.TbFuncionario'  is null.");
+            }
+            _context.TbFuncionario.Add(tbFuncionario);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TbFuncionarioExists(tbFuncionario.Matricula))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTbFuncionario", new { id = tbFuncionario.Matricula }, tbFuncionario);
+        }
+
+        // DELETE: api/TbFuncionarios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTbFuncionario(int id)
+        {
+            if (_context.TbFuncionario == null)
+            {
+                return NotFound();
+            }
+            var tbFuncionario = await _context.TbFuncionario.FindAsync(id);
+            if (tbFuncionario == null)
+            {
+                return NotFound();
+            }
+
+            _context.TbFuncionario.Remove(tbFuncionario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TbFuncionarioExists(int id)
+        {
+            return (_context.TbFuncionario?.Any(e => e.Matricula == id)).GetValueOrDefault();
+        }
+    }
+}

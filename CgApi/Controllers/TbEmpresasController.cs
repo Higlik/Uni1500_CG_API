cc9ce4337872327e468677f8673aa57e489689d0
Deivide <dejs_silva@hotmail.com>
2022-08-18 14:27:44 -0300

    Controller_Empresas_Paises

---
+++ CgApi/Controllers/TbEmpresasController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CgApi;
+
+namespace CgApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TbEmpresasController : ControllerBase
+    {
+        private readonly cursodev_grupo2Context _context;
+
+        public TbEmpresasController(cursodev_grupo2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TbEmpresas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TbEmpresa>>> GetTbEmpresa()
+        {
+          if (_context.TbEmpresa == null)
+          {
+              return NotFound();
+          }
+            return await _context.TbEmpresa.ToListAsync();
+        }
+
+        // GET: api/TbEmpresas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TbEmpresa>> GetTbEmpresa(int id)
+        {
+          if (_context.TbEmpresa == null)
+          {
+              return NotFound();
+          }
+            var tbEmpresa = await _context.TbEmpresa.FindAsync(id);
+
+            if (tbEmpresa == null)
+            {
+                return NotFound();
+            }
+
+            return tbEmpresa;
+        }
+
+        // PUT: api/TbEmpresas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTbEmpresa(int id, TbEmpresa tbEmpresa)
+        {
+            if (id != tbEmpresa.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tbEmpresa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TbEmpresaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TbEmpresas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TbEmpresa>> PostTbEmpresa(TbEmpresa tbEmpresa)
+        {
+          if (_context.TbEmpresa == null)
+          {
+              return Problem("Entity set 'cursodev_grupo2Context.TbEmpresa'  is null.");
+          }
+            _context.TbEmpresa.Add(tbEmpresa);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TbEmpresaExists(tbEmpresa.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTbEmpresa", new { id = tbEmpresa.Id }, tbEmpresa);
+        }
+
+        // DELETE: api/TbEmpresas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTbEmpresa(int id)
+        {
+            if (_context.TbEmpresa == null)
+            {
+                return NotFound();
+            }
+            var tbEmpresa = await _context.TbEmpresa.FindAsync(id);
+            if (tbEmpresa == null)
+            {
+                return NotFound();
+            }
+
+            _context.TbEmpresa.Remove(tbEmpresa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TbEmpresaExists(int id)
+        {
+            return (_context.TbEmpresa?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ CgApi/Controllers/TbPaisesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CgApi;
+
+namespace CgApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TbPaisesController : ControllerBase
+    {
+        private readonly cursodev_grupo2Context _context;
+
+        public TbPaisesController(cursodev_grupo2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TbPaises
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TbPais>>> GetTbPais()
+        {
+          if (_context.TbPais == null)
+          {
+              return NotFound();
+          }
+            return await _context.TbPais.ToListAsync();
+        }
+
+        // GET: api/TbPaises/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TbPais>> GetTbPais(int id)
+        {
+          if (_context.TbPais == null)
+          {
+              return NotFound();
+          }
+            var tbPais = await _context.TbPais.FindAsync(id);
+
+            if (tbPais == null)
+            {
+                return NotFound();
+            }
+
+            return tbPais;
+        }
+
+        // PUT: api/TbPaises/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTbPais(int id, TbPais tbPais)
+        {
+            if (id != tbPais.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tbPais).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TbPaisExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TbPaises
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TbPais>> PostTbPais(TbPais tbPais)
+        {
+          if (_context.TbPais == null)
+          {
+              return Problem("Entity set 'cursodev_grupo2Context.TbPais'  is null.");
+          }
+            _context.TbPais.Add(tbPais);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TbPaisExists(tbPais.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTbPais", new { id = tbPais.Id }, tbPais);
+        }
+
+        // DELETE: api/TbPaises/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTbPais(int id)
+        {
+            if (_context.TbPais == null)
+            {
+                return NotFound();
+            }
+            var tbPais = await _context.TbPais.FindAsync(id);
+            if (tbPais == null)
+            {
+                return NotFound();
+            }
+
+            _context.TbPais.Remove(tbPais);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TbPaisExists(int id)
+        {
+            return (_context.TbPais?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
